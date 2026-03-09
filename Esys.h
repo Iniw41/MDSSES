a49ca60f14d56c6adcfee50272281baffada9513
Francis Rainier Cutamora <169155198+Iniw41@users.noreply.github.com>
2026-03-09 10:51:18 +0800

    Add function prototypes and include headers in Esys.h

--- Esys.h
+++ Esys.h
@@ -1,6 +1,8 @@
 #ifndef ESYS_H
 #define ESYS_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct Node {
@@ -12,10 +14,22 @@ typedef struct Node {
     struct Node* next;
 } StudentList;
 
-// Define 'Node' as a pointer to that struct
 typedef StudentList* Node;
 
-// PROTOTYPE: Must return 'Node' (which is StudentList*)
+// Function Prototypes
 Node createStudent(int id, char* name, char* prog, int year, float gpa);
+int isDuplicate(Node head, int id);
+void regularEnrollment(Node* head, int id, char* name, char* prog, int year, float gpa);
+void priorityEnrollment(Node* head, int id, char* name, char* prog, int year, float gpa);
+void manualInsert(Node* head, int id, char* name, char* prog, int year, float gpa, int pos);
+void lateEnrollment(Node* head, int id, char* name, char* prog, int year, float gpa);
+void displayAll(Node head);
+void searchStudent(Node head, int id);
+void deleteStudent(Node* head, int id);
+int countStudents(Node head);
+void promoteYear(Node* head);
+void computeAverageGPA(Node head);
+void displayTopN(Node head, int n);
+void reverseList(Node* head);
 
 #endif
